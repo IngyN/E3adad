21bee87c0bef9465c07b9ec59d95cb085a5d47aa
hagerrady13 <hagerrady133@gmail.com>
2015-07-08 14:16:21 +0200

    last_estimation added to shared preferences

--- app/src/main/java/edu/aucegypt/ingyn/e3adad/models/SharedPref.java
+++ app/src/main/java/edu/aucegypt/ingyn/e3adad/models/SharedPref.java
@@ -10,17 +10,22 @@ public class SharedPref {
     static String user_id;
     static String device_id;
     static Context context;
+    static String last_estimation;
 
     public SharedPref (Context context)
     {
         user_data = context.getSharedPreferences("user_data", Context.MODE_PRIVATE);
         editor = user_data.edit();
     }
-
     public SharedPref(Context context,String user_id,String device_id){
         SharedPref.context = context;
         SharedPref.user_id = user_id;
         SharedPref.device_id = device_id;
+        user_data = context.getSharedPreferences("user_data", Context.MODE_PRIVATE);
+        editor = user_data.edit();
+    }
+    public SharedPref(Context context,String last_estimation){
+        SharedPref.last_estimation = last_estimation;
         user_data = context.getSharedPreferences("user_data", Context.MODE_PRIVATE);
         editor = user_data.edit();
     }
@@ -33,6 +38,8 @@ public class SharedPref {
         editor.commit();
         editor.putString("device_id", device_id);
         editor.commit();
+         editor.putString("last_estimation", last_estimation);
+         editor.commit();
     }
 
     static public String getUser_id(){
@@ -51,6 +58,14 @@ public class SharedPref {
             return device_id;
             }
         else return null;
+
+    }
+    static public int getLast_estimation() {
+        if (user_data.contains("last_estimation")){
+            last_estimation = user_data.getString("last_estimation", null);
+            return Integer.parseInt(last_estimation);
+        }
+        else return 0;
 
     }
 
