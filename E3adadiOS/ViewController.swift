b98c2c6fc6e1e0cb26db3c4a702dcd0294ac9739
IngyN <ingyesa@aucegypt.edu>
2015-07-13 20:59:36 +0200

    New http method for connection

--- E3adadiOS/ViewController.swift
+++ E3adadiOS/ViewController.swift
@@ -7,19 +7,85 @@
 //
 
 import UIKit
+//import SwiftyJSON
+
 // class CustomTableViewCell: UITableViewCell{
     
 class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
    // var items: [String] = ["","E-3adad", "is", "an" , "awesome","application"]
     var items = [String]()
+    var api_url = "http://baseetta.com/hatem/e3adad/history.php?user_id=47"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        getData()
+        get_data(api_url);
         self.items.append("Hello People")
         PrintAll()
         // Do any additional setup after loading the view, typically from a nib.
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+
+    func get_data (url:String)
+    {
+        let httpMethod = "GET"
+        let timeout = 15
+        let url = NSURL(string: url)
+        let urlRequest = NSMutableURLRequest(URL: url!,
+        cachePolicy: .ReloadIgnoringLocalAndRemoteCacheData,
+        timeoutInterval: 15.0)
+        let queue = NSOperationQueue()
+
+        NSURLConnection.sendAsynchronousRequest(
+                                      urlRequest,
+                                      queue: queue,
+                                      completionHandler: {(response: NSURLResponse!, data: NSData!, error: NSError!) in
+                 if data.length > 0 && error == nil{
+
+                     let json = NSString(data: data, encoding: NSASCIIStringEncoding)
+                     self.extract_json(json!)
+                     self.refresh_table();
+//                    println("anything!!!!");
+                    self.items.append("Ay shit tayeb?");
+
+                 } else if data.length == 0 && error == nil{
+
+                     println("Nothing was downloaded")
+
+                 } else if error != nil
+                 {
+                     println("Error happened = \(error)")
+                 }
+             }
+         )
+    }
+
+    func refresh_table(){
+
+     dispatch_async(dispatch_get_main_queue(), {
+             self.tableView.reloadData()
+            return
+         })
+    }
+
+    func extract_json (data : NSString!)
+    {
+            let json = JSON(data)
+            let Submissions = json["results"]
+            for (index, object) in Submissions {
+                    let submission_id: String = object["submission_id"].stringValue
+                    let reading: String = object["reading"].stringValue
+                    let submission_date: String = object["submission_date"].stringValue
+                    let is_paid: String = object["is_paid"].stringValue
+                    self.items.append(reading)
+                    self.items.append(submission_date)
+                    self.items.append(is_paid)
+                    println("submission_id = \(submission_id) ")
+                    println("reading = \(reading) ")
+                    println("submission_date = \(submission_date)) ")
+                    println("is_paid = \(is_paid) ")
+                    self.items.append("Hello People")
+                }
     }
   //  func getData(url: String , parameters: [String:AnyObject]){
     func getData(){
